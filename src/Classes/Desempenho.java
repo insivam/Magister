2ce88fd364374a09de92da7650d1c60282e848a1
insivam <88207216+insivam@users.noreply.github.com>
2022-04-08 15:38:38 -0300

    Update Desempenho.java

--- src/Classes/Desempenho.java
+++ src/Classes/Desempenho.java
@@ -1,16 +1,40 @@
 package Classes;
 
-public class Desempenho {
+import java.util.Scanner;
 
+public class Professor {
+	
     /*
-     * Irá passar por uma lista de alunos
-     * e deixara o professor seleionar um desempenho
-     * de uma lista de desempenhos (ex: Bom, Regular, Ruim)
-     * O professor tambem pode adicionar uma sugestão para
-     * melhora de desempenho (Não é obrigatorio)
+     * Cria um sistema de Login e Senha do professor
      * 
-     * Vai se chamar calculaDesempenho
-     * e vai receber a matrizAlunos
+     * caso a senha esteja incorreta,
+     * deve-se pedir a senha novamente.
+     * 
+     * Vai se chamar login
      */
+	Scanner entrada = new Scanner(System.in);
+	
+	   public static boolean login() {
+		   Scanner entrada = new Scanner(System.in);
+		   String login ;
+		   String senha ;
+	
+		   do {
+	            System.out.print("Digite seu login: ");
+	            login = entrada.next();
+	            entrada.nextLine();
 
+	            System.out.print("Digite sua senha: ");
+	            senha = entrada.next();
+
+	            if (login.equals("Professor") && senha.equals("1969")) {
+	                System.out.println("Entrando no Sistema.....");
+	                return true;
+	            } else {
+	                System.out.println("Login ou senha incorretos!");
+	                return false;
+	            }
+	        } while (!login.equals("Professor") && !senha.equals("1969"));
+	    	
+	    	}
 }
