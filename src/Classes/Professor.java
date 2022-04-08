dee43e4a563d3da24b01d402632b0b3fc7ac8518
insivam <88207216+insivam@users.noreply.github.com>
2022-04-08 17:05:06 -0300

    Update Professor.java

--- src/Classes/Professor.java
+++ src/Classes/Professor.java
@@ -3,7 +3,7 @@ package Classes;
 import java.util.Scanner;
 
 public class Professor {
-	
+
     /*
      * Cria um sistema de Login e Senha do professor
      * 
@@ -12,29 +12,29 @@ public class Professor {
      * 
      * Vai se chamar login
      */
-	Scanner entrada = new Scanner(System.in);
-	
-	   public static boolean login() {
-		   Scanner entrada = new Scanner(System.in);
-		   String login ;
-		   String senha ;
-	
-		   do {
-	            System.out.print("Digite seu login: ");
-	            login = entrada.next();
-	            entrada.nextLine();
+    Scanner entrada = new Scanner(System.in);
 
-	            System.out.print("Digite sua senha: ");
-	            senha = entrada.next();
+    public static boolean login() {
+        Scanner entrada = new Scanner(System.in);
+        String login;
+        String senha;
 
-	            if (login.equals("Professor") && senha.equals("1969")) {
-	                System.out.println("Entrando no Sistema.....");
-	                return true;
-	            } else {
-	                System.out.println("Login ou senha incorretos!");
-	                return false;
-	            }
-	        } while (!login.equals("Professor") && !senha.equals("1969"));
-	    	
-	    	}
+        do {
+            System.out.print("Digite seu login: ");
+            login = entrada.next();
+            entrada.nextLine();
+
+            System.out.print("Digite sua senha: ");
+            senha = entrada.next();
+
+            if (login.equals("Professor") && senha.equals("1969")) {
+                System.out.println("Entrando no Sistema.....");
+                return true;
+            } else {
+                System.out.println("Login ou senha incorretos!");
+                return false;
+            }
+        } while (!login.equals("Professor") && !senha.equals("1969"));
+
+    }
 }
